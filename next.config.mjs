0126a7aadf1e8492beba053d5d6b7409cfd08484
antick-arch <e241003@grad.iiuc.ac.bd>
2026-05-08 22:12:26 +0600

    Enhance About page layout and add new image assets

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+  // https://i.ibb.co/p6SfQMcq/antick.png
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'i.ibb.co',
+        port: '',
+        pathname: '/p6SfQMcq/**',
+        search: 'antick chandra kuri',
+      },
+    ],
+  },
   reactCompiler: true,
 };
 
--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,9 +1,13 @@
+import Image from "next/image";
 const AboutPage = () => {
   
     return (
-        <div>
-            <h2>This is about page</h2>
-            
+        <div className="container mx-auto">
+            <h2 className="my-5 text-center font-bold text-4xl">This is about page</h2>
+            <div className="flex gap-5">
+                <Image src="/work-2.png" alt="this is photography" width={500} height={300} />
+                <Image src={"https://i.ibb.co/p6SfQMcq/antick.png"} alt="this is antick chandra kuri" width={500} height={300} className="rounded-2xl"></Image>
+            </div>
         </div>
     );
 };
